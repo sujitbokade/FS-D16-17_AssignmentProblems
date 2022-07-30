918b9bde1f7781345833f5b409999b5968bb160c
sujit <sujitbokade2014@gmail.com>
2022-07-30 18:08:46 +0530

    [Sujit]Insertion Sort Program

--- src/com/bridgelabz/problems/InsertionSort.java
+++ src/com/bridgelabz/problems/InsertionSort.java
@@ -1,17 +1,19 @@
 package com.bridgelabz.problems;
 
+import java.lang.reflect.Array;
+
 public class InsertionSort {
     public static void main(String[] args) {
-        int []array={8,6,9,5,12,15};
+        String [] array={"Rohit","Neha","Rajveer","Sonali","Raj"};
 
         for(int i=0;i<array.length;i++){
             System.out.print(array[i]+" ");
         }
 
         for(int i=1;i<array.length;i++){
-            int current=array[i];
+            String current=array[i];
             int j=i-1;
-            while(j>=0 && current<array[j]){
+            while(j>=0 && current.compareTo(array[j])<0){
                 array[j+1]=array[j];
                 j--;
             }
